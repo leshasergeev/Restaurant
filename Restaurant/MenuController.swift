15aaf77360812dd567075553bd762d67d9ae6f0d
iBamboola <41113234+iBamboola@users.noreply.github.com>
2021-05-26 17:11:30 +0300

    Networking was added

---
+++ Restaurant/MenuController.swift
@@ -0,0 +1,98 @@
+//
+//  MenuController.swift
+//  Restaurant
+//
+//  Created by Алексей Сергеев on 26.05.2021.
+//  Copyright © 2021 Алексей Сергеев. All rights reserved.
+//
+
+import Foundation
+
+
+class MenuController {
+    let baseURL = URL(string: "http://localhost:8080/")!
+    
+    func fetchCategories(completion: @escaping (Result<[String], Error>) -> Void) {
+        let categoriesURL = baseURL.appendingPathComponent("categories")
+        
+        let dataTask = URLSession.shared.dataTask(with: categoriesURL) { (data, response, error) in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let categoriesResponse = try jsonDecoder.decode(CategoriesResponse.self, from: data)
+                    completion(.success(categoriesResponse.categories))
+                } catch {
+                    completion(.failure(error))
+                }
+                
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        dataTask.resume()
+    }
+    
+    func fetchMenuItems(forCategory categoryName: String, completion: @escaping (Result<[MenuItem], Error>) -> Void) {
+        let baseMenuURL = baseURL.appendingPathComponent("menu")
+        
+        var components = URLComponents(url: baseMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        
+        let menuURL = components.url!
+        
+        let dataTask = URLSession.shared.dataTask(with: menuURL) { (data, response, error) in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let menuResponse = try jsonDecoder.decode(MenuResponse.self, from: data)
+                    completion(.success(menuResponse.items))
+                } catch {
+                    completion(.failure(error))
+                }
+                
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        dataTask.resume()
+    }
+    
+    typealias MinutesToPrepare = Int
+    
+    func submitOrder(forMenuIDs menuIDs: [Int], completion: @escaping (Result<MinutesToPrepare, Error>) -> Void){
+        let orderURL = baseURL.appendingPathComponent("order")
+        
+        // change the request method
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        
+        let data = ["menuIDs": menuIDs]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        
+        request.httpBody = jsonData
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let orderResponse = try jsonDecoder.decode(OrderResponse.self, from: data)
+                    completion(.success(orderResponse.prepTime))
+                } catch {
+                    completion(.failure(error))
+                }
+                
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        dataTask.resume()
+    }
+}
+
+
+
+
+
